cff68ce614dd57f2addc2fb5987fa7d0440f2491
Pilar <pilaransa31@gmail.com>
2022-05-11 22:49:57 -0300

    agregando contenido 0.0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from "./AppComponent";
 import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { AcercadeComponent } from './componentes/acercade/acercade.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EncabezadoComponent
+    EncabezadoComponent,
+    AcercadeComponent,
+    ExperienciaComponent
   ],
   imports: [
     BrowserModule
